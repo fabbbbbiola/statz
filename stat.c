1899f4e9fd59f4bc9664a07414b34a2da390f0b3
fabiola <fradosav@stuy.edu>
2017-11-01 20:23:09 -0400

    fixed stuff

--- stat.c
+++ stat.c
@@ -1,19 +1,17 @@
+#include <sys/stat.h>
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <sys/stat.h>
-#include <unistd.h>
 
 int main(){
 
   struct stat alf;
-  stat("file", &alf);
+  stat("file.txt", &alf);
   
-  printf("file size: %lu\n", sizeof(alf.st_size));
-  printf("mode: %d\n", alf.st_mode);
-  printf("last access time: %s\n", ctime(alf.st_atime));
+  printf("file size: %lld\n", alf.st_size);
+  printf("mode: %o\n", alf.st_mode); 
+  printf("last access time: %s\n", ctime(&(alf.st_atime)));
 
 
-
-
+  return 0;
 }
